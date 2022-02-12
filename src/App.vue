085df036a8f80079ea3b6aa73b155f1d01b64c23
SchawnnDev <iperskill@gmail.com>
2022-02-12 10:57:41 +0100

    ajout navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <presentation/>
+      <NavBar/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Presentation from "@/components/Presentation"
+import NavBar from "@/components/NavBar"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Presentation,
+    NavBar
   }
 }
 </script>
 
 <style>
+* {
+  font-family: 'Karla', sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.container {
+  margin-left: 20%;
+  margin-right: 20%;
+
+  border: #2c3e50 solid 2px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+
+
 </style>
